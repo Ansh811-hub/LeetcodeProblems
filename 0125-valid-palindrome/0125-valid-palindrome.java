869a3ccca55c055592ed166ac4faf6e5d85f0030
Ansh Bhardwaj <www.anshbhardwaj1505@gmail.com>
2026-07-01 00:20:13 +0530

    Time: 134 ms (5.18%), Space: 48.3 MB (5.21%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str2 = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        String rev = "";
+
+    for (int i = str2.length() - 1; i >= 0; i--) {
+    rev += str2.charAt(i);
+    }
+         return str2.equals(rev);
+    }
+}
